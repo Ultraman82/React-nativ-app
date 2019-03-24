b44286866629d2551a2b22bc6811c411f8238992
Edgar Jeong <chjeong@e1z2r3p10.42.us.org>
2019-03-24 15:03:05 -0700

    Social sharing

--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, ScrollView, FlatList, Modal, StyleSheet, Button, Alert, PanResponder } from 'react-native';
+import { Text, View, ScrollView, FlatList, Modal, StyleSheet, Button, Alert, PanResponder, Share } from 'react-native';
 import { Card, Icon, Rating, Input } from 'react-native-elements';
 import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
@@ -71,6 +71,15 @@ const panResponder = PanResponder.create({
     }
 })
 
+const shareDish = (title, message, url) => {
+  Share.share({
+      title: title,
+      message: title + ': ' + message + ' ' + url,
+      url: url
+  },{
+      dialogTitle: 'Share ' + title
+  })
+}
 
 if (dish != null) {
     return(
@@ -86,6 +95,7 @@ if (dish != null) {
             name={props.favorite ? 'heart' : 'heart-o'}
             type="font-awesome"
             color="#f50"
+            
             onPress={() =>
               props.favorite ? console.log("Already favorite") : props.onPress()
             }            
@@ -93,10 +103,20 @@ if (dish != null) {
           <Icon
             raised            
             reverse
-            name={'edit'}
+            name={'pencil'}
             type="font-awesome"
             color="#512DA8"
+            
             onPress={() => props.toggleModal()}
+          />
+          <Icon
+            raised            
+            reverse
+            name='share'
+            type="font-awesome"
+            color="#51D2AA"
+            
+            onPress={() => shareDish(dish.name, dish.description, baseUrl + dish.image)}
           />
         </View>
         </Card>
